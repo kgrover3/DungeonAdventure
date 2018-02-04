0972ff1e3588de8ba0c5cee5d575f0eea5c3f0d9
Ken Grover <Kenneth.Grover@snhu.edu>
2018-02-04 18:57:24 -0500

    changes to file structure

--- DungeonAdventure/Data/Weapons.py
+++
@@ -1,7 +0,0 @@
-#Weapon List
-
-stick = {'name':'Stick','damage':2, 'description':'A wooden stick.... Maybe it will help'}
-shortSword = {'name':'Short Soward','damage':4, 'description':'A short sword of reasonable quality'}
-
-
-weaponList = [stick, shortSword]
