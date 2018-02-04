e80a0c16ca25b884f251426a6fcc342453075f52
Kenneth Grover <kgroveriii@gmail.com>
2018-02-04 18:37:44 -0500

    Added Project Start Files

---
+++ DungeonAdventure/Data/Weapons.py
@@ -0,0 +1,7 @@
+#Weapon List
+
+stick = {'name':'Stick','damage':2, 'description':'A wooden stick.... Maybe it will help'}
+shortSword = {'name':'Short Soward','damage':4, 'description':'A short sword of reasonable quality'}
+
+
+weaponList = [stick, shortSword]
